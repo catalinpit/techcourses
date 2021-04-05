cbb2d969b25b8acd872e8a4a124ef51d0fd3d44f
catalinpit <catalinpit@gmail.com>
2021-04-05 09:42:50 +0300

    Connected FE to BE and cleaned the frontend

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,68 +1,48 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">frontend</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
-export default {}
+export default {
+  asyncData({ $axios, error }) {
+    const body = {
+      query: `
+        query GetCourses {
+          allCourses {
+              name
+              description
+              releaseDate
+              author
+              comments {
+                id
+              }
+          }
+        }
+      `,
+      variables: {},
+    }
+
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    return $axios
+      .post('http://localhost:3000/admin/api', body, options)
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.',
+        })
+      })
+  },
+}
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
